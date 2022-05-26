1fd69e007265cca1861dd668795902451394ac88
Ray <rui.zhangchn@qq.com>
2022-05-27 01:49:31 +0800

    init

--- src/ast.c
+++ src/ast.c
@@ -1,247 +1,248 @@
 #include "def.h"
-struct node *mknode(int kind, struct node *first, struct node *second, struct node *third, int pos)
-{
-  struct node *T = (struct node *)malloc(sizeof(struct node));
-  T->kind = kind;
-  T->ptr[0] = first;
-  T->ptr[1] = second;
-  T->ptr[2] = third;
-  T->pos = pos;
+
+struct node * mknode(int kind,struct node *first,struct node *second, struct node *third,int pos ) {
+  struct node *T=(struct node *)malloc(sizeof(struct node));
+  T->kind=kind;
+  T->ptr[0]=first;
+  T->ptr[1]=second;
+  T->ptr[2]=third;
+  T->pos=pos;
   return T;
 }
 
-struct node *mknode4(int kind, struct node *first, struct node *second, struct node *third, struct node *fourth, int pos)
-{
-  struct node *T = (struct node *)malloc(sizeof(struct node));
-  T->kind = kind;
-  T->ptr[0] = first;
-  T->ptr[1] = second;
-  T->ptr[2] = third;
-  T->ptr[3] = fourth;
-  T->pos = pos;
-  return T;
-}
-
-void display(struct node *T, int indent)
-{ //对抽象语法树的先根遍历
+void display(struct node *T, int indent)  {  //对抽象语法树的先根遍历
   int i = 1;
   struct node *T0;
-  if (T)
-  {
-    switch (T->kind)
-    {
-    case EXT_DEF_LIST:
-      display(T->ptr[0], indent); //显示该外部定义列表中的第一个
-      display(T->ptr[1], indent); //显示该外部定义列表中的其它外部定义
+  if (T) {
+	  switch (T->kind) {
+	  case EXT_DEF_LIST:
+      display(T->ptr[0], indent);  //显示该外部定义列表中的第一个
+      display(T->ptr[1], indent);  //显示该外部定义列表中的其它外部定义
       break;
     case EXT_VAR_DEF:
-      printf("%*c|--外部变量定义:\n", indent, ' ');
-      display(T->ptr[0], indent + 3); //显示外部变量类型
-      printf("%*c|--变量名:\n", indent + 3, ' ');
-      display(T->ptr[1], indent + 6); //显示变量列表
+      printf("%*c外部变量定义:\n", indent, ' ');
+      display(T->ptr[0], indent+3);  //显示外部变量类型
+      printf("%*c定义的变量:\n",indent+3,' ');
+      display(T->ptr[1], indent+6);  //显示变量列表
       break;
-    case TYPE:
-      printf("%*c|--类型: %s\n", indent, ' ', T->type_id);
+    case EXT_CONST_VAR_DEF:
+      printf("%*c外部常量定义:\n", indent, ' ');
+      display(T->ptr[0], indent+3);  //显示外部常量类型
+      printf("%*c定义的常量:\n",indent+3,' ');
+      display(T->ptr[1], indent+6);  //显示常量列表
       break;
-    case EXT_DEC_LIST:
-      display(T->ptr[0], indent); //依次显示外部变量名，
-      display(T->ptr[1], indent); //后续还有相同的，仅显示语法树此处理代码可以和类似代码合并
-      break;
-    case FUNC_DEF:
-      printf("%*c|--函数定义:\n", indent, ' ');
-      display(T->ptr[0], indent + 3); //显示函数返回类型
-      display(T->ptr[1], indent + 3); //显示函数名和参数
-      display(T->ptr[2], indent + 3); //显示函数体
-      break;
-    case FUNC_DEC:
-      printf("%*c|--函数名:%s\n", indent, ' ', T->type_id);
-      if (T->ptr[0])
-      {
-        printf("%*c|--函数形参:\n", indent, ' ');
-        display(T->ptr[0], indent + 3); //显示函数参数列表
+    case TERM:
+      if (T->ptr[0]) {
+        printf("%*c数组: ", indent, ' ');
+      } else {
+        printf("%*cID: ", indent, ' ');
       }
-      else
-        printf("%*c|--无参函数\n", indent, ' ');
+      printf("%s\n", T->type_id);
+      display(T->ptr[0], indent+3);
       break;
-    case PARAM_LIST:
-      display(T->ptr[0], indent); //依次显示全部参数类型和名称，
-      display(T->ptr[1], indent);
-      break;
-    case PARAM_DEC:
-      printf("%*c|--类型:%s, 参数名:%s\n", indent, ' ',
-             T->ptr[0]->type == INT ? "int" : "float", T->ptr[1]->type_id);
-      break;
-    case EXP_STMT:
-      printf("%*c|--表达式语句:\n", indent, ' ');
-      display(T->ptr[0], indent + 3);
-      break;
-    case RETURN:
-      printf("%*c|--返回语句:\n", indent, ' ');
-      display(T->ptr[0], indent + 3);
-      break;
-    case COMP_STM:
-      printf("%*c|--复合语句:\n", indent, ' ');
-      // printf("%*c|--复合语句的变量定义:\n",indent+3,' ');
-      // display(T->ptr[0],indent+6);      //显示定义部分
-      // printf("%*c|--复合语句的语句部分:\n",indent+3,' ');
-      // display(T->ptr[1],indent+6);      //显示语句部分
-      display(T->ptr[0], indent);
-      break;
-    case BLOCK_LIST:
-      display(T->ptr[0], indent); //显示第一条语句
-      display(T->ptr[1], indent); //显示剩下语句
-      break;
-    case BLOCK_DEF:
-      printf("%*c|--复合语句的变量定义:\n", indent + 3, ' ');
-      display(T->ptr[0], indent + 6);
-      break;
-    case BLOCK_STM:
-      printf("%*c|--复合语句的语句部分:\n", indent + 3, ' ');
-      display(T->ptr[0], indent + 6);
-      break;
-    case STM_LIST:
-      display(T->ptr[0], indent); //显示第一条语句
-      display(T->ptr[1], indent); //显示剩下语句
-      break;
-    case WHILE:
-      printf("%*c|--循环语句(While):\n", indent, ' ');
-      printf("%*c|--循环条件:\n", indent + 3, ' ');
-      display(T->ptr[0], indent + 6); //显示循环条件
-      printf("%*c|--循环体:\n", indent + 3, ' ');
-      display(T->ptr[1], indent + 6); //显示循环体
-      break;
-    case FOR:
-      printf("%*c|--循环语句(For):\n", indent, ' ');
-      printf("%*c|--循环条件(变量定义):\n", indent + 3, ' ');
-      display(T->ptr[0], indent + 6); //显示循环条件
-      printf("%*c|--循环条件(结束判断):\n", indent + 3, ' ');
-      display(T->ptr[1], indent + 6); //显示循环条件
-      printf("%*c|--循环条件(变量改变):\n", indent + 3, ' ');
-      display(T->ptr[2], indent + 6); //显示循环条件
-      printf("%*c|--循环体:\n", indent + 3, ' ');
-      display(T->ptr[3], indent + 6); //显示循环体
-      break;
-
-    case BREAK:
-      printf("%*c|--break\n", indent, ' ');
-      break;
-    case CONTINUE:
-      printf("%*c|--continue\n", indent, ' ');
-      break;
-
-    case IF_THEN:
-      printf("%*c|--条件语句(IF_THEN):\n", indent, ' ');
-      printf("%*c|--条件:\n", indent + 3, ' ');
-      display(T->ptr[0], indent + 6); //显示条件
-      printf("%*c|--IF子句:\n", indent + 3, ' ');
-      display(T->ptr[1], indent + 6); //显示if子句
-      break;
-    case IF_THEN_ELSE:
-      printf("%*c|--条件语句(IF_THEN_ELSE):\n", indent, ' ');
-      printf("%*c|--条件:\n", indent + 3, ' ');
-      display(T->ptr[0], indent + 6); //显示条件
-      printf("%*c|--IF子句:\n", indent + 3, ' ');
-      display(T->ptr[1], indent + 6); //显示if子句
-      printf("%*c|--ELSE子句:\n", indent + 3, ' ');
-      display(T->ptr[2], indent + 6); //显示else子句
-      break;
-    case DEF_LIST:
-      display(T->ptr[0], indent); //显示该局部变量定义列表中的第一个
-      display(T->ptr[1], indent); //显示其它局部变量定义
-      break;
-    case VAR_DEF:
-      printf("%*c|--LOCAL VAR_NAME:\n", indent, ' ');
-      display(T->ptr[0], indent + 3); //显示变量类型
-      display(T->ptr[1], indent + 3); //显示该定义的全部变量名
-      break;
-    case DEC_LIST:
-      printf("%*c|--VAR_NAME:\n", indent, ' ');
+    case ARRAYS:
       T0 = T;
-      while (T0)
-      {
-        if (T0->ptr[0]->kind == ID)
-          printf("%*c|-- %s\n", indent + 3, ' ', T0->ptr[0]->type_id);
-        else if (T0->ptr[0]->kind == ASSIGNOP)
-        {
-          printf("%*c|-- %s ASSIGNOP\n ", indent + 3, ' ', T0->ptr[0]->ptr[0]->type_id);
-          //显示初始化表达式
-          display(T0->ptr[0]->ptr[1], indent + strlen(T0->ptr[0]->ptr[0]->type_id) + 4);
-        }
+      i = 1;
+      while (T0 != NULL) {
+        printf("%*c第%d维: \n", indent, ' ', i++);
+        display(T0->ptr[0], indent+3);
         T0 = T0->ptr[1];
       }
       break;
-    case ID:
-      printf("%*c|--ID: %s\n", indent, ' ', T->type_id);
+	  case TYPE:
+	  case VOID:
+      printf("%*c类型: %s\n",indent,' ',T->type_id);
+      break;
+    case EXT_DEC_LIST:
+      display(T->ptr[0], indent);  //依次显示外部变量名，
+      display(T->ptr[1], indent);  //后续还有相同的，仅显示语法树此处理代码可以和类似代码合并
+      break;
+    case VAR_DEC:
+      printf("%*c变量名: %s\n",indent,' ',T->type_id);
+      if (T->ptr[0]) {
+        printf("%*c它的初值:\n",indent+3,' ');
+        display(T->ptr[0], indent+6);  //表达式初值
+      }
+      break;
+    case FUNC_DEF:
+      printf("%*c函数定义：\n",indent,' ');
+      display(T->ptr[0],indent+3);      //显示函数返回类型
+      display(T->ptr[1],indent+3);      //显示函数名和参数
+      display(T->ptr[2],indent+3);      //显示函数体
+      break;
+	  case FUNC_DEC:
+      printf("%*c函数名：%s\n",indent,' ',T->type_id);
+      if (T->ptr[0]) {
+        printf("%*c函数形参：\n",indent,' ');
+        display(T->ptr[0],indent+3);  //显示函数参数列表
+        }
+      else printf("%*c无参函数\n",indent+3,' ');
+      break;
+	  case PARAM_LIST:    
+      display(T->ptr[0],indent);     //依次显示全部参数类型和名称，
+      display(T->ptr[1],indent);
+      break;
+  	case PARAM_DEC:     
+      printf("%*c类型：%s, 参数名：%s\n", indent,' ', T->ptr[0]->type==INT?"int": "float",T->ptr[1]->type_id);
+      break;
+	  case EXP_STMT:
+      printf("%*c表达式语句：\n",indent,' ');
+      display(T->ptr[0],indent+3);
+      break;
+	  case RETURN:
+      if (T->ptr[0]) {
+        printf("%*c返回语句：\n",indent,' ');
+        display(T->ptr[0], indent+3);
+      } else {
+        printf("%*c空返回语句\n",indent,' ');
+      }
+      break;
+    case CONTINUE_STMT:
+      printf("%*c继续语句\n",indent,' ');
+      break;
+    case BREAK_STMT:
+      printf("%*c打断语句\n",indent,' ');
+      break;
+    case COMP_STM:
+      if (T->ptr[0]) {
+        printf("%*c复合语句: \n",indent,' ');
+        display(T->ptr[0],indent+3);
+      } else {
+        printf("%*c空复合语句\n",indent,' ');
+      }
+      break;
+	  case STM_DEF_LIST:
+      display(T->ptr[0],indent);      //显示第一条语句
+      display(T->ptr[1],indent);        //显示剩下语句
+      break;
+	  case WHILE:
+      printf("%*c循环语句：\n",indent,' ');
+      printf("%*c循环条件：\n",indent+3,' ');
+      display(T->ptr[0],indent+6);      //显示循环条件
+      printf("%*c循环体：\n",indent+3,' ');
+      display(T->ptr[1],indent+6);      //显示循环体
+      break;
+    case FOR_STMT:
+	    printf("%*cfor 循环语句: \n",indent,' ');
+      display(T->ptr[0], indent+3);
+      printf("%*c循环体: \n",indent+3,' ');
+      display(T->ptr[1], indent+6);
+      break;
+    case FOR_ARGS:
+	    printf("%*cfor 循环起始表达式: \n",indent,' ');
+      if (T->ptr[0]) {
+        display(T->ptr[0], indent+3);
+      } else {
+	      printf("%*c无\n",indent+3,' ');
+      }
+      printf("%*cfor 循环条件表达式: \n",indent,' ');
+      if (T->ptr[1]) {
+        display(T->ptr[1], indent+3);
+      } else {
+	      printf("%*c无\n",indent+3,' ');
+      }
+      printf("%*cfor 循环第三表达式: \n",indent,' ');
+      if (T->ptr[2]) {
+        display(T->ptr[2], indent+3);
+      } else {
+	      printf("%*c无\n",indent+3,' ');
+      }
+      break;
+    case IF_THEN:
+      printf("%*c条件语句(IF_THEN)：\n", indent, ' ');
+      printf("%*c条件：\n", indent + 3, ' ');
+      display(T->ptr[0], indent + 6); //显示条件
+      printf("%*cIF子句：\n", indent + 3, ' ');
+      display(T->ptr[1], indent + 6); //显示if子句
+      break;
+    case IF_THEN_ELSE:
+      printf("%*c条件语句(IF_THEN_ELSE)：\n", indent, ' ');
+      printf("%*c条件：\n", indent + 3, ' ');
+      display(T->ptr[0], indent + 6); //显示条件
+      printf("%*cIF子句：\n", indent + 3, ' ');
+      display(T->ptr[1], indent + 6); //显示if子句
+      printf("%*cELSE子句：\n", indent + 3, ' ');
+      display(T->ptr[2], indent + 6); //显示else子句
+      break;
+    case VAR_DEF:
+      printf("%*c局部变量定义: \n",indent,' ');
+      display(T->ptr[0], indent+3);   //显示变量类型
+      printf("%*c定义的变量:\n",indent+3,' ');
+      display(T->ptr[1], indent+6);   //显示该定义的全部变量名
+      break;
+    case DEC_LIST:
+      display(T->ptr[0], indent);
+      display(T->ptr[1], indent);
+      break;
+    case NODE_ID:
+      printf("%*cID： %s\n", indent, ' ', T->type_id);
       break;
     case INT:
-      printf("%*c|--INT:%d\n", indent, ' ', T->type_int);
+      printf("%*cINT：%d\n", indent, ' ', T->type_int);
       break;
     case FLOAT:
-      printf("%*c|--FLAOT:%f\n", indent, ' ', T->type_float);
+      printf("%*cFLAOT：%f\n", indent, ' ', T->type_float);
       break;
-    case ASSIGNOP:
+    case ASSIGN:
     case AND:
     case OR:
     case RELOP:
-    case PLUS:
+    case ADD:
     case MINUS:
-    case STAR:
+    case MUL:
     case DIV:
     case MOD:
-    case INC:
-      printf("%*c|--%s\n", indent, ' ', T->type_id);
+      printf("%*c%s\n", indent, ' ', T->type_id);
       display(T->ptr[0], indent + 3);
       display(T->ptr[1], indent + 3);
       break;
     case NOT:
     case UMINUS:
-      printf("%*c|--%s\n", indent, ' ', T->type_id);
+      printf("%*c%s\n", indent, ' ', T->type_id);
       display(T->ptr[0], indent + 3);
       break;
     case FUNC_CALL:
-      printf("%*c|--函数调用:\n", indent, ' ');
-      printf("%*c|--函数名:%s\n", indent + 3, ' ', T->type_id);
+      printf("%*c函数调用：\n", indent, ' ');
+      printf("%*c函数名：%s\n", indent + 3, ' ', T->type_id);
       display(T->ptr[0], indent + 3);
       break;
     case ARGS:
       i = 1;
-      while (T)
-      { // ARGS表示实际参数表达式序列结点，其第一棵子树为其一个实际参数表达式，第二棵子树为剩下的。
+      while (T) { // ARGS表示实际参数表达式序列结点，其第一棵子树为其一个实际参数表达式，第二棵子树为剩下的。
         struct node *T0 = T->ptr[0];
-        printf("%*c|--第%d个实际参数表达式:\n", indent, ' ', i++);
+        printf("%*c第%d个实际参数表达式：\n", indent, ' ', i++);
         display(T0, indent + 3);
         T = T->ptr[1];
       }
-      //                  printf("%*c第%d个实际参数表达式:\n",indent,' ',i);
+      //                  printf("%*c第%d个实际参数表达式：\n",indent,' ',i);
       //                  display(T,indent+3);
-      printf("\n");
+      // printf("\n");
       break;
-    case ARRAY:
-      printf("%*c|--数组变量\n", indent, ' ');
-      printf("%*c|--数组变量名:%s\n", indent + 3, ' ', T->type_id);
-      printf("%*c|--数组维度:\n", indent + 3, ' ');
-      display(T->ptr[0], indent + 6);
+    case SELF_ADD_EXP:
+      printf("%*c后置自增: \n",indent,' ');
+      printf("%*c变量: %s\n", indent+3,' ', T->type_id);
       break;
-    case DIM:
-      i = 1;
-      while (T)
-      {
-        if (T->type == INT)
-        {
-          printf("%*c|--第%d维:%d\n", indent, ' ', i++, T->type_int);
-        }
-        else if (T->type == ID)
-        {
-          printf("%*c|--第%d维:%s\n", indent, ' ', i++, T->type_id);
-        }
-        T = T->ptr[0];
-      }
-      printf("\n");
-      break;
-    case INIT_LIST:
-      printf("%*c|--列表初始化\n", indent, ' ');
-      display(T->ptr[0], indent + 3);
+    case SELF_MINUS_EXP:
+      printf("%*c后置自减: \n",indent,' ');
+      printf("%*c变量: %s\n", indent+3,' ' , T->type_id);
       break;
     }
   }
 }
+
+// 显示当前符号表中的内容,请补充代码
+void DisplaySymbolTable()
+{
+    int i;
+    printf("\t\t***Symbol Table***\n");
+    printf("---------------------------------------------------\n");
+    printf("%s\t%s\t%s\t%s\t%s\t%s\n","Index","Name","Level","Type","Flag","Param_num");
+    printf("---------------------------------------------------\n");
+    for(; ; ){
+ 
+ 
+     }
+    printf("---------------------------------------------------\n");
+    printf("\n");
+}
+
--- src/def.h
+++ src/def.h
@@ -2,124 +2,133 @@
 #include "stdlib.h"
 #include "string.h"
 #include "stdarg.h"
-#include "parser.tab.h"
+#include "sysy.tab.h"
 
 enum node_kind
 {
-    EXT_DEF_LIST,
-    EXT_VAR_DEF,
-    FUNC_DEF,
-    FUNC_DEC,
-    EXT_DEC_LIST,
-    PARAM_LIST,
-    PARAM_DEC,
-    VAR_DEF,
-    DEC_LIST,
-    DEF_LIST,
-    COMP_STM,
-    STM_LIST,
-    EXP_STMT,
-    IF_THEN,
-    IF_THEN_ELSE,
-    FUNC_CALL,
-    ARGS,
-    FUNCTION,
-    PARAM,
-    ARG,
-    CALL,
-    LABEL,
-    GOTO,
-    JLT,
-    JLE,
-    JGT,
-    JGE,
-    EQ,
-    NEQ,
-    BLOCK_LIST,
-    BLOCK_DEF,
-    BLOCK_STM,
-    ARRAY,
-    DIM,
-    INIT_LIST
+  EXT_DEF_LIST,
+  EXT_VAR_DEF,
+  EXT_CONST_VAR_DEF,
+  FUNC_DEF,
+  FUNC_DEC,
+  EXT_DEC_LIST,
+  PARAM_LIST,
+  PARAM_DEC,
+  VAR_DEF,
+  VAR_DEC,
+  DEC_LIST,
+  COMP_STM,
+  STM_DEF_LIST,
+  EXP_STMT,
+  IF_THEN,
+  IF_THEN_ELSE,
+  FUNC_CALL,
+  ARGS,
+  CONTINUE_STMT,
+  BREAK_STMT,
+  FOR_STMT,
+  FOR_ARGS,
+  FUNCTION,
+  PARAM,
+  ARG,
+  CALL,
+  LABEL,
+  GOTO,
+  JLT,
+  JLE,
+  JGT,
+  JGE,
+  EQ,
+  NEQ,
+  NODE_ID,
+  SELF_ADD_EXP,
+  SELF_MINUS_EXP,
+  TERM,
+  ARRAYS
 };
+
+
 #define MAXLENGTH 1000     //定义符号表的大小
 #define DX 3 * sizeof(int) //活动记录控制信息需要的单元数
+char filename[50];
+int lev;
+
 //以下语法树结点类型、三地址结点类型等定义仅供参考，实验时一定要根据自己的理解来定义
 struct opn
 {
-    int kind; //标识操作的类型
-    int type; //标识操作数的类型
-    union
-    {
-        int const_int;     //整常数值，立即数
-        float const_float; //浮点常数值，立即数
-        char const_char;   //字符常数值，立即数
-        char id[33];       //变量或临时变量的别名或标号字符串
-    };
-    int level;  //变量的层号，0表示是全局变量，数据保存在静态数据区
-    int offset; //变量单元偏移量，或函数在符号表的定义位置序号，目标代码生成时用
+  int kind; //标识操作的类型
+  int type; //标识操作数的类型
+  union
+  {
+    int const_int;     //整常数值，立即数
+    float const_float; //浮点常数值，立即数
+    char const_char;   //字符常数值，立即数
+    char id[33];       //变量或临时变量的别名或标号字符串
+  };
+  int level;  //变量的层号，0表示是全局变量，数据保存在静态数据区
+  int offset; //变量单元偏移量，或函数在符号表的定义位置序号，目标代码生成时用
 };
 
 struct codenode
-{                                  //三地址TAC代码结点,采用双向循环链表存放中间语言代码
-    int op;                        // TAC代码的运算符种类
-    struct opn opn1, opn2, result; // 2个操作数和运算结果
-    struct codenode *next, *prior;
+{                                //三地址TAC代码结点,采用双向循环链表存放中间语言代码
+  int op;                        //TAC代码的运算符种类
+  struct opn opn1, opn2, result; //2个操作数和运算结果
+  struct codenode *next, *prior;
 };
 
 struct node
-{                        //以下对结点属性定义没有考虑存储效率，只是简单地列出要用到的一些属性
-    enum node_kind kind; //结点类型
-    union
-    {
-        char type_id[33]; //由标识符生成的叶结点
-        int type_int;     //由整常数生成的叶结点
-        float type_float; //由浮点常数生成的叶结点
-    };
-    struct node *ptr[4];        //子树指针，由kind确定有多少棵子树
-    int level;                  //层号
-    int place;                  //表示结点对应的变量或运算结果临时变量在符号表的位置序号
-    char Etrue[15], Efalse[15]; //对布尔表达式的翻译时，真假转移目标的标号
-    char Snext[15];             //该结点对应语句执行后的下一条语句位置标号
-    struct codenode *code;      //该结点中间代码链表头指针
-    char op[10];
-    int type;   //结点对应值的类型
-    int pos;    //语法单位所在位置行号
-    int offset; //偏移量
-    int width;  //各种数据占用的字节数
+{                      //以下对结点属性定义没有考虑存储效率，只是简单地列出要用到的一些属性
+  enum node_kind kind; //结点类型
+  union
+  {
+    char type_id[33]; //由标识符生成的叶结点
+    int type_int;     //由整常数生成的叶结点
+    float type_float; //由浮点常数生成的叶结点
+  };
+  struct node *ptr[3];        //子树指针，由kind确定有多少棵子树
+  int level;                  //层号
+  int place;                  //表示结点对应的变量或运算结果临时变量在符号表的位置序号
+  char Etrue[15], Efalse[15]; //对布尔表达式的翻译时，真假转移目标的标号
+  char Snext[15];             //该结点对应语句执行后的下一条语句位置标号
+  struct codenode *code;      //该结点中间代码链表头指针
+  char op[10];
+  int type;   //结点对应值的类型
+  int pos;    //语法单位所在位置行号
+  int offset; //偏移量
+  int width;  //各种数据占用的字节数
 };
 
 struct symbol
-{                   //这里只列出了一个符号表项的部分属性，没考虑属性间的互斥
-    char name[33];  //变量或函数名
-    int level;      //层号，外部变量名或函数名层号为0，形参名为1，每到1个复合语句层号加1，退出减1
-    int type;       //变量类型或函数返回值类型
-    int paramnum;   //形式参数个数
-    char alias[10]; //别名，为解决嵌套层次使用，使得每一个数据名称唯一
-    char flag;      //符号标记，函数：'F'  变量：'V'   参数：'P'  临时变量：'T'
-    char offset;    //外部变量和局部变量在其静态数据区或活动记录中的偏移量
-                    //或函数活动记录大小，目标代码生成时使用
-    //其它...
-};
+{                 //这里只列出了一个符号表项的部分属性，没考虑属性间的互斥
+  char name[33];  //变量或函数名
+  int level;      //层号，外部变量名或函数名层号为0，形参名为1，每到1个复合语句层号加1，退出减1
+  int type;       //变量类型或函数返回值类型
+  int paramnum;   //形式参数个数
+  char alias[10]; //别名，为解决嵌套层次使用，使得每一个数据名称唯一
+  char flag;      //符号标记，函数：'F'  变量：'V'   参数：'P'  临时变量：'T'
+  char offset;    //外部变量和局部变量在其静态数据区或活动记录中的偏移量
+                  //或函数活动记录大小，目标代码生成时使用
+  //其它...
+} ;
+
 //符号表，是一个顺序栈，index初值为0
 struct symboltable
 {
-    struct symbol symbols[MAXLENGTH];
-    int index;
+  struct symbol symbols[MAXLENGTH];
+  int index;
 } symbolTable;
 
 struct symbol_scope_begin
 { /*当前作用域的符号在符号表的起始位置序号,这是一个栈结构，/每到达一个复合语句，将符号表的index值进栈，离开复合语句时，取其退栈值修改符号表的index值，完成删除该复合语句中的所有变量和临时变量*/
-    int TX[30];
-    int top;
+  int TX[30];
+  int top;
 } symbol_scope_TX;
 
-struct node *mknode(int kind, struct node *first, struct node *second, struct node *third, int pos);
-struct node *mknode4(int kind, struct node *first, struct node *second, struct node *third, struct node *fourth, int pos);
-
 // 为语义分析和代码生成预留
-void semantic_Analysis0(struct node *T);
-void semantic_Analysis(struct node *T);
-void boolExp(struct node *T);
-void Exp(struct node *T);
-void objectCode(struct codenode *head);
+struct node *mknode(int kind,struct node *first,struct node *second, struct node *third,int pos );
+void display(struct node *T, int indent);
+void DisplaySymbolTable();
+//int Semantic_Analysis(struct node* T,int type,int level,char flag,int command);
+//void boolExp(struct node *T);
+//void Exp(struct node *T);
+//void objectCode(struct codenode *head);
